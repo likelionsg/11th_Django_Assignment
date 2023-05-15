8cd6ff053318f4fb2064372b2544b2a0ff2d9a1e
Als-ET <et_oh@naver.com>
2023-05-15 19:33:42 +0900

    안녕하세요"

---
+++ django_project/accounts/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import auth
+
+def login(request):
+    if(request.method == 'POST'):
+        userid = request.POST['id']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=userid, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('list_page')
+        else:
+            return render(request, 'login_form.html')
+    else:#GET
+        return render(request, 'login_form.html')
+
+
+
+
+
+
+
+
+def detail(request, pk):
+    item = get_object_or_404(Shopping, pk=pk)
+    return render(request, 'detail.html', {'item':item,})
+
+# 쇼핑 아이템(Shopping) 작성 html을 보여주거나 저장해주는 함수
+def create(request):
+    if(request.method == 'POST'):
+        item = Shopping()
+        item.name = request.POST['name']
+        item.price = request.POST['price']
+        item.count = request.POST['count']
+        store_name = request.POST['store']
+        item.store = get_object_or_404(Store, name=store_name)
+        item.save()
+    return render(request, 'create.html')
+
+def formcreate(request):
+    if request.method == 'POST':
+        form = ShoppingForm(request.POST)
+        if form.is_valid():
+            item = Shopping()
+            item.name = form.cleaned_data['name']
+            item.price = form.cleaned_data['price']
+            item.count = form.cleaned_data['count']
+            store_name = form.cleaned_data['store']
+            item.store = get_object_or_404(Store, name=store_name)
+            item.save()
+            return redirect('list_page')
+    else:
+        form = ShoppingForm()
+    return render(request, 'form_create.html', {'form':form})
+
+def modelformcreate(request):
+    if request.method == 'POST':
+        form = ShoppingModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_page')
+    else:
+        form = ShoppingModelForm()
+    return render(request, 'form_create.html', {'form':form})
