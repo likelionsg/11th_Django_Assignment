fa4ce3a553b2c8308c4ccc0ccec03dde76b987cd
Als-ET <et_oh@naver.com>
2023-05-15 21:00:37 +0900

    5월 15일 페이지, 검색 기능 추가

--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import auth
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 def login(request):
     if(request.method == 'POST'):
@@ -14,51 +17,16 @@ def login(request):
     else:#GET
         return render(request, 'login_form.html')
 
-
-
-
-
-
-
-
-def detail(request, pk):
-    item = get_object_or_404(Shopping, pk=pk)
-    return render(request, 'detail.html', {'item':item,})
-
-# 쇼핑 아이템(Shopping) 작성 html을 보여주거나 저장해주는 함수
-def create(request):
+def signup(request):
     if(request.method == 'POST'):
-        item = Shopping()
-        item.name = request.POST['name']
-        item.price = request.POST['price']
-        item.count = request.POST['count']
-        store_name = request.POST['store']
-        item.store = get_object_or_404(Store, name=store_name)
-        item.save()
-    return render(request, 'create.html')
+        user = User.objects.create_user(username = request.POST['id'],
+                                 password = request.POST['password'],
+                                 phone = request.POST['phone'])
+        auth.login(request, user)
+        return redirect('list_page')
+    else:#GET
+        return render(request, 'signup.html')
 
-def formcreate(request):
-    if request.method == 'POST':
-        form = ShoppingForm(request.POST)
-        if form.is_valid():
-            item = Shopping()
-            item.name = form.cleaned_data['name']
-            item.price = form.cleaned_data['price']
-            item.count = form.cleaned_data['count']
-            store_name = form.cleaned_data['store']
-            item.store = get_object_or_404(Store, name=store_name)
-            item.save()
-            return redirect('list_page')
-    else:
-        form = ShoppingForm()
-    return render(request, 'form_create.html', {'form':form})
-
-def modelformcreate(request):
-    if request.method == 'POST':
-        form = ShoppingModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('list_page')
-    else:
-        form = ShoppingModelForm()
-    return render(request, 'form_create.html', {'form':form})
+def logout(request):
+        auth.logout(request)
+        return redirect('list_page')
