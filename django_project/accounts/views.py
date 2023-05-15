8086f1e248b533d88385e6022bc2b6db21d0dd4b
JTH <nex_david@naver.com>
2023-05-16 01:09:10 +0900

    JTH_first

---
+++ django_project/accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        userid = request.POST['id']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=userid, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('list_page')
+        else:
+            return render(request, 'login_form.html')  # redirect('login') 
+    else:  # GET
+        return render(request, 'login_form.html')
+
+
+def signup(request):
+    if request.method == 'POST': # 유저 객체 만들어줘야함
+        user = User.objects.create_user(username = request.POST['id'],
+                                password = request.POST['password'],
+                                phone = request.POST['phone'])
+        auth.login(request, user)
+        return redirect('list_page')
+
+    else:
+        return render(request, 'signup.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('list_page')
