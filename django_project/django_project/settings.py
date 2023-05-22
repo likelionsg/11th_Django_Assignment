7a3df5683577bdd2a05110f3c30e51510d5f04a7
Koyujin815 <meoi12@naver.com>
2023-05-22 10:54:18 +0900

    django_project done

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     'shopping',
     'store',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'		
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# custom user model
+
+AUTH_USER_MODEL = 'accounts.User'
